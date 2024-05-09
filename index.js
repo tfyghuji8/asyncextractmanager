c82e35b4d61184a544aee2c42b9fa3a965459f52
tfyghuji8 <js1ZALDA123@hotmail.com>
2024-05-09 18:12:23 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function searchInsert(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
   }
-  return left;
+  return i === s.length;
 }
